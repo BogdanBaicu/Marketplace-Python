ae76fb944bb419bbbdc58e8a7cbe64417fd71565
Bogdan <bogdan_bbx@yahoo.com>
2023-04-18 17:12:26 +0300

    added consumer

--- tema/consumer.py
+++ tema/consumer.py
@@ -7,6 +7,7 @@ March 2021
 """
 
 from threading import Thread
+from time import sleep
 
 
 class Consumer(Thread):
@@ -36,7 +37,37 @@ class Consumer(Thread):
         self.carts = carts
         self.marketplace = marketplace
         self.retry_wait_time = retry_wait_time
+        self.cart = None
+        self.name = kwargs["name"]
 
 
     def run(self):
-        pass
+        for cart in self.carts:
+            # generate id
+            cart_id = self.marketplace.new_cart()
+
+            # execute instructions for each product
+            for prod in cart:
+                quant = prod["quantity"]
+
+                while quant:
+                    available = None
+
+                    if prod["type"] == "add":
+                        available = self.marketplace.add_to_cart(cart_id, prod["product"])
+
+                        if available:
+                            quant -= 1
+                        else:
+                            # if product is not available, retry aflter retry_wait_time
+                            sleep(self.retry_wait_time)
+                    elif prod["type"] == "remove":
+                        available = self.marketplace.remove_from_cart(cart_id, prod["product"])
+                        quant -= 1
+            
+            # place order
+            prod = self.marketplace.place_order(cart_id)
+
+            # print the output
+            for item in prod:
+                print('{} bought {}'.format(self.name, item))
