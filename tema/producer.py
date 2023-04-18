8211e7f8159d3cba5eb5d34dc22620c4ed10051c
Bogdan <bogdan_bbx@yahoo.com>
2023-04-18 13:42:59 +0300

    added producer

--- tema/producer.py
+++ tema/producer.py
@@ -7,7 +7,7 @@ March 2021
 """
 
 from threading import Thread
-
+from time import sleep
 
 class Producer(Thread):
     """
@@ -36,6 +36,25 @@ class Producer(Thread):
         self.products = products
         self.marketplace = marketplace
         self.republish_wait_time = republish_wait_time
+        self.producer_id = None
 
     def run(self):
-        pass
+        #generate producer id
+        self.producer_id = self.marketplace.register_producer()
+
+        while True:
+            for prod_data in self.products:
+                counter = prod_data[1]
+
+                while counter:
+                    # try to publish the product
+                    published = self.marketplace.publish(self.producer_id, prod_data[0])
+
+                    # if it's already published, sleep
+                    if published is True:
+                        sleep(prod_data[2])
+                        counter -= 1
+
+                    # try to republish
+                    else:
+                        sleep(self.republish_wait_time)
